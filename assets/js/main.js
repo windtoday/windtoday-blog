3a9ff9d2cfb82af18b05687ac8a1b88ca963b364
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-02-25 17:45:16 +0100

    WIP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
 $(function() {
-  var search = $("#search")
-  var suggestions = $('#search-suggestions')
-  search.typed({
-    stringsElement: suggestions,
+  var searchEl = $("#search")
+  var suggestionsEl = $('#search-suggestions')
+  var formEl = $("form")
+  var searchQuery;
+
+  searchEl.typed({
+    stringsElement: suggestionsEl,
     attr: "placeholder",
     typeSpeed: 50,
     backDelay: 1000,
     loop: true
+  });
+
+  searchEl.on("change paste keyup", function() {
+    var query = $(this).val()
+    if (!query) return
+    searchQuery = query
+  });
+
+  formEl.submit(function(event) {
+    event.preventDefault();
+    var url = 'https://windtoday.co/?query=' + searchQuery
+    window.open(url)
   });
 });
