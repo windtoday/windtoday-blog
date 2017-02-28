6e0c544681f097e8f57ad3fde7e39ba446710835
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-02-28 08:51:31 +0100

    Fix domain search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,28 @@
-$(function() {
-  var searchEl = $("#search")
+/* global $ */
+
+$(function () {
+  var searchEl = $('#search')
   var suggestionsEl = $('#search-suggestions')
-  var formEl = $("form")
-  var searchQuery;
+  var formEl = $('form')
+  var searchQuery
 
   searchEl.typed({
     stringsElement: suggestionsEl,
-    attr: "placeholder",
+    attr: 'placeholder',
     typeSpeed: 50,
     backDelay: 1000,
     loop: true
-  });
+  })
 
-  searchEl.on("change paste keyup", function() {
+  searchEl.on('change paste keyup', function () {
     var query = $(this).val()
     if (!query) return
     searchQuery = query
-  });
+  })
 
-  formEl.submit(function(event) {
-    event.preventDefault();
-    var url = 'https://windtoday.co/?query=' + searchQuery
+  formEl.submit(function (event) {
+    event.preventDefault()
+    var url = 'https://market.windtoday.co/?query=' + searchQuery
     window.open(url)
-  });
-});
+  })
+})
