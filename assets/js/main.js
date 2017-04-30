62c8aeaea5fd056eb8486ad389b3f78668bb1062
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-05-01 01:25:57 +0200

    Add payment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/* global $ */
+/* global $, StripeCheckout, swal */
 
 $(function () {
   $('.jquery-background-video').bgVideo({
@@ -27,5 +27,48 @@ $(function () {
     event.preventDefault()
     var url = 'https://market.windtoday.co/?query=' + searchQuery
     window.open(url)
+  })
+})
+
+$(function () {
+  var handler = StripeCheckout.configure({
+    key: 'pk_live_3Wuqx47tGykhuLnbJtFHto4W',
+    image: 'http://windtoday.co/assets/img/logo.jpg',
+    locale: 'auto',
+    token: function (token) {
+      $.post('https://windtoday-payment-tbimdwiaxb.now.sh/payment', {
+        token: token, plan: 'small'
+      }, function (res) {
+        var status = res.status
+        if (status === 'success') {
+          swal('Payment received', 'Now you\'re part of the ecosystem', 'success')
+        } else {
+          swal('Oops...', 'Something went wrong!', 'error')
+        }
+      })
+    }
+  })
+
+  function createPlan (selector, opts) {
+    var description = opts.name + ' Plan'
+    var amount = opts.amount
+
+    $(selector).click(function (evt) {
+      evt.preventDefault()
+      handler.open({
+        name: 'Windtoday Marketplace',
+        description: description,
+        amount: amount,
+        currency: 'eur'
+      })
+    })
+  }
+
+  createPlan('.small-btn', {name: 'Small', amount: 1199})
+  createPlan('.medium-btn', {name: 'Medium', amount: 2399})
+  createPlan('.large-btn', {name: 'Large', amount: 6999})
+
+  window.addEventListener('popstate', function () {
+    handler.close()
   })
 })
