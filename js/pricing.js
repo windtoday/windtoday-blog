dc14514f7a615dab163ddeb061fa3652fc4cb2eb
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-05-13 15:03:07 +0200

    Logo over HTTPS

--- js/pricing.js
+++ js/pricing.js
@@ -3,7 +3,7 @@
 $(function () {
   var handler = StripeCheckout.configure({
     key: 'pk_live_3Wuqx47tGykhuLnbJtFHto4W',
-    image: 'http://windtoday.co/logo.jpg',
+    image: 'https://windtoday.co/logo.jpg',
     locale: 'auto',
     token: function (token) {
       $.post('https://windtoday.now.sh/payment', {
