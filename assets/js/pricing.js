d5a71d15855ef02b6c93a916d012b07ec0e4534a
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-23 14:43:28 +0200

    Load functions as soon as possible

--- assets/js/pricing.js
+++ assets/js/pricing.js
@@ -1,41 +1,41 @@
 /* global $, StripeCheckout, swal */
 
-$(function () {
-  function createCheckout (planId) {
-    return StripeCheckout.configure({
-      key: 'pk_live_3Wuqx47tGykhuLnbJtFHto4W',
-      image: 'https://blog.windtoday.co/logo.jpg',
-      locale: 'auto',
-      token: function (token) {
-        $.post('https://windtoday.now.sh/payment', {
-          token: token, plan: planId
-        }, function (res) {
-          var status = res.status
-          if (status === 'success') {
-            swal('Payment received', 'Now you\'re part of the ecosystem', 'success')
-          } else {
-            swal('Oops...', 'Something went wrong!', 'error')
-          }
-        })
-      }
-    })
-  }
-
-  function createPlan (selector, opts) {
-    var checkout = createCheckout(opts.planId)
-    window.addEventListener('popstate', checkout.close)
-
-    $(selector).click(function (evt) {
-      evt.preventDefault()
-      checkout.open({
-        name: 'Windtoday Marketplace',
-        description: opts.name + ' Plan',
-        amount: opts.amount,
-        currency: 'eur'
+function createCheckout (planId) {
+  return StripeCheckout.configure({
+    key: 'pk_live_3Wuqx47tGykhuLnbJtFHto4W',
+    image: 'https://blog.windtoday.co/logo.jpg',
+    locale: 'auto',
+    token: function (token) {
+      $.post('https://windtoday.now.sh/payment', {
+        token: token, plan: planId
+      }, function (res) {
+        var status = res.status
+        if (status === 'success') {
+          swal('Payment received', 'Now you\'re part of the ecosystem', 'success')
+        } else {
+          swal('Oops...', 'Something went wrong!', 'error')
+        }
       })
-    })
-  }
+    }
+  })
+}
 
+function createPlan (selector, opts) {
+  var checkout = createCheckout(opts.planId)
+  window.addEventListener('popstate', checkout.close)
+
+  $(selector).click(function (evt) {
+    evt.preventDefault()
+    checkout.open({
+      name: 'Windtoday Marketplace',
+      description: opts.name + ' Plan',
+      amount: opts.amount,
+      currency: 'eur'
+    })
+  })
+}
+
+$(function () {
   createPlan('.small-btn', {name: 'Small', amount: 1900, planId: 'small_v2'})
   createPlan('.medium-btn', {name: 'Medium', amount: 2600, planId: 'medium'})
   createPlan('.large-btn', {name: 'Large', amount: 9900, planId: 'large'})
