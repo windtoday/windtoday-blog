af25e0fad9fe06882b76bdc82d4f10b78a0f45d1
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-23 14:40:32 +0200

    Update payments

--- assets/js/pricing.js
+++ assets/js/pricing.js
@@ -1,42 +1,42 @@
 /* global $, StripeCheckout, swal */
 
 $(function () {
-  var handler = StripeCheckout.configure({
-    key: 'pk_live_3Wuqx47tGykhuLnbJtFHto4W',
-    image: 'https://blog.windtoday.co/logo.jpg',
-    locale: 'auto',
-    token: function (token) {
-      $.post('https://windtoday.now.sh/payment', {
-        token: token, plan: 'small'
-      }, function (res) {
-        var status = res.status
-        if (status === 'success') {
-          swal('Payment received', 'Now you\'re part of the ecosystem', 'success')
-        } else {
-          swal('Oops...', 'Something went wrong!', 'error')
-        }
-      })
-    }
-  })
+  function createCheckout (planId) {
+    return StripeCheckout.configure({
+      key: 'pk_live_3Wuqx47tGykhuLnbJtFHto4W',
+      image: 'https://blog.windtoday.co/logo.jpg',
+      locale: 'auto',
+      token: function (token) {
+        $.post('https://windtoday.now.sh/payment', {
+          token: token, plan: planId
+        }, function (res) {
+          var status = res.status
+          if (status === 'success') {
+            swal('Payment received', 'Now you\'re part of the ecosystem', 'success')
+          } else {
+            swal('Oops...', 'Something went wrong!', 'error')
+          }
+        })
+      }
+    })
+  }
 
   function createPlan (selector, opts) {
-    var description = opts.name + ' Plan'
-    var amount = opts.amount
+    var checkout = createCheckout(opts.planId)
+    window.addEventListener('popstate', checkout.close)
 
     $(selector).click(function (evt) {
       evt.preventDefault()
-      handler.open({
+      checkout.open({
         name: 'Windtoday Marketplace',
-        description: description,
-        amount: amount,
+        description: opts.name + ' Plan',
+        amount: opts.amount,
         currency: 'eur'
       })
     })
   }
 
-  createPlan('.small-btn', {name: 'Small', amount: 1400})
-  createPlan('.medium-btn', {name: 'Medium', amount: 2600})
-  createPlan('.large-btn', {name: 'Large', amount: 9900})
-
-  window.addEventListener('popstate', handler.close)
+  createPlan('.small-btn', {name: 'Small', amount: 1900, planId: 'small_v2'})
+  createPlan('.medium-btn', {name: 'Medium', amount: 2600, planId: 'medium'})
+  createPlan('.large-btn', {name: 'Large', amount: 9900, planId: 'large'})
 })
